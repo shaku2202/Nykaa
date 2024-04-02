0eddcb5c3c3ae2c555be439f3f721da327cff096
Shashank-2202 <shashank.psit057@gmail.com>
2024-04-03 00:42:45 +0530

    Final

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 // Connect to MongoDB using Mongoose
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/kanban_board_db', {
+mongoose.connect('mongodb+srv://shashank:shashank@cluster0.6r1yal4.mongodb.net/Nykaa?retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
